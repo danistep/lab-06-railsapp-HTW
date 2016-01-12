07862855b8f03a72e8446881f11818c978715463
Daniil Stepanov <danistep@mail.ru>
2016-01-12 23:04:50 +0100

    stepped back to change price to decimal

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160112215315) do
+ActiveRecord::Schema.define(version: 20140707195241) do
 
   create_table "customers", force: true do |t|
     t.string   "name"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20160112215315) do
     t.string   "status"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "price"
   end
 
   create_table "pumpkins", force: true do |t|
